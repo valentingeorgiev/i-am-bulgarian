913f8ad8d95af8161465331b83237df9836da173
Valentin Georgiev <valio.v.georgiev@gmail.com>
2019-04-30 19:02:22 +0300

    fix logout

--- app/utils/Logout.js
+++ app/utils/Logout.js
@@ -2,7 +2,7 @@ import {AsyncStorage} from 'react-native';
 
 export async function logout() {
   const value = await AsyncStorage.getItem('user');
-  if(value !== null) {
+  if(value) {
     const status = await deleteUserMetaToken(JSON.parse(value).id, JSON.parse(value).authToken);
     if(status) {
       await AsyncStorage.removeItem('user');    
@@ -16,15 +16,18 @@ async function deleteUserMetaToken(userID, authToken) {
   formData.append('user_id', userID);
   formData.append('auth_token', authToken);
   
-  return fetch('https://i-am-bulgarian.000webhostapp.com/authentication.php', {
-    method: 'POST',
-    body: formData
-  })
-  .then((response) => response.json())
-  .then((responseJson) => {
-    return responseJson.status;
-  })
-  .catch((error) => {
+  try {
+    const response = await fetch('https://i-am-bulgarian.000webhostapp.com/authentication.php', {
+      method: 'POST',
+      body: formData
+    });
+    
+    const responseJson = await response.json();
+    if(responseJson.status){
+      return responseJson.status;
+    }
+  }
+  catch(error) {
     console.error(error);
-  });
+  }
 }
