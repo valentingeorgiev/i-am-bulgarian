c3494a980c4a96adbe0025d77981e1412880fbf8
Valentin Georgiev <valio.v.georgiev@gmail.com>
2019-05-15 21:53:48 +0300

    fix TouchableOpacity

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image} from 'react-native';
+import { View, Image, TouchableOpacity} from 'react-native';
 import { Link } from 'react-router-native';
 
 import nav from '../styles/navigation';
@@ -10,10 +10,10 @@ export default class Navigation extends React.Component {
       <View style={nav.navContainer}>
 
         <View style={nav.navLeft}>
-          <Link to='/home' style={nav.navItem }>
+          <Link component={TouchableOpacity} to='/home' style={nav.navItem }>
             <Image style={nav.navIcon} source={require('../assets/img/icons/home-solid.png')}/>
           </Link>
-          <Link to='/most-visited-landmarks' style={nav.navItem }>
+          <Link component={TouchableOpacity} to='/most-visited-landmarks' style={nav.navItem }>
             <Image style={nav.navIcon} source={require('../assets/img/icons/map-marked-alt-solid.png')}/>
           </Link>
         </View>
@@ -21,7 +21,7 @@ export default class Navigation extends React.Component {
         <View style={nav.navCenter}>
           <View style={nav.navLeftBorder}></View>
           <View style={nav.navCenterBorder}>
-            <Link to='/ar-camera' style={nav.arItem}>
+            <Link component={TouchableOpacity} to='/ar-camera' style={nav.arItem}>
               <Image style={nav.arNavIcon} source={require('../assets/img/icons/ar-tag.png')}/>
             </Link>
           </View>
@@ -29,10 +29,10 @@ export default class Navigation extends React.Component {
         </View>
         
         <View style={nav.navRight}>
-          <Link to='/rankings' style={nav.navItem}>
+          <Link component={TouchableOpacity} to='/rankings' style={nav.navItem}>
             <Image style={nav.navIcon} source={require('../assets/img/icons/medal-solid.png')}/>
           </Link>
-          <Link to='/user-profile' style={nav.navItem}>
+          <Link component={TouchableOpacity} to='/user-profile' style={nav.navItem}>
             <Image style={nav.navIcon} source={require('../assets/img/icons/user-solid.png')}/>
           </Link>
         </View>
